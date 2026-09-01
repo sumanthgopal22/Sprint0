6bcca6cbfbea02412f4e68a55bf4343516b2faac
conta <conta@SUMANTH_PC>
2026-08-31 21:04:47 -0400

    Add project files.

---
+++ Sprint0/Game1.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Sprint0
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Texture2D _link;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            _link = Content.Load<Texture2D>("Link/link");
+            
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // Begin the sprite batch to prepare for rendering
+            _spriteBatch.Begin();
+
+            // Draw Link at the at top left of the screen
+            _spriteBatch.Draw(_link, Vector2.Zero, Color.White);
+
+            // Always end the sprite batch after drawing
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
